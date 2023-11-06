d7fba239de420cdebf9b59f2396eeb61db1f6a6d
Vinny1027 <gm22rl@brocku.ca>
2023-11-05 21:45:24 -0500

    created and finished lab8

---
+++ week8/week8c2.js
@@ -0,0 +1,19 @@
+function start(){
+  document.getElementById("blue").style.color="blue";
+  document.getElementById("green").style.color="green";
+  document.getElementById("red").style.color="red";
+}
+
+
+
+function mix(){
+
+  var first = "green";
+  var second = "orange";
+  var third = "purple";
+
+  document.getElementById("blue").style.color="green";
+  document.getElementById("green").style.color="orange";
+  document.getElementById("red").style.color="purple";
+
+}
---
+++ week8/week8c3.js
@@ -0,0 +1,9 @@
+
+
+function theRest()
+{
+  document.getElementById("output0").innerHTML="Output Zero here";
+  document.getElementById("output1").innerHTML="Output One Here";
+	document.getElementById("output2").innerHTML="Output Two Here";
+	document.getElementById("output3").innerHTML="Output Three Here";
+}
