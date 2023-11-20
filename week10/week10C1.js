81142eb43997fe4e0235a971daf4510d60d36ddd
Vinny1027 <gm22rl@brocku.ca>
2023-11-20 00:54:32 -0500

    did week10

---
+++ week10/week10C1.js
@@ -0,0 +1,31 @@
+function add1and2(){
+  var num1 = parseInt(document.getElementById('num1').value);
+  var num2 = parseInt(document.getElementById('num2').value);
+  var res = num1+num2;
+  document.write(res);
+
+}
+
+function subt1and2(){
+  var num1 = parseInt(document.getElementById('num1').value);
+  var num2 = parseInt(document.getElementById('num2').value);
+  var res = num1-num2;
+  document.write(res);
+
+}
+
+function multi1and2(){
+  var num1 = parseInt(document.getElementById('num1').value);
+  var num2 = parseInt(document.getElementById('num2').value);
+  var res = num1*num2;
+  document.write(res);
+
+}
+
+function divi1and2(){
+  var num1 = parseInt(document.getElementById('num1').value);
+  var num2 = parseInt(document.getElementById('num2').value);
+  var res = num1/num2;
+  document.write(res);
+
+}
