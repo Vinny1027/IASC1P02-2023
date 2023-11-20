2fd678cadd07ec9d8e5bb9be3aa7220b77cf2042
Vinny1027 <gm22rl@brocku.ca>
2023-11-20 00:54:46 -0500

    played with week9

--- week9E3.js
+++ week9E3.js
@@ -6,6 +6,6 @@ function addFunction()
   var x=parseInt(document.getElementById('val1').value);
   var y=parseInt(document.getElementById('val2').value);
   var res=x + y;
-  console.log(res);
+  document.write(res);
 
 }
