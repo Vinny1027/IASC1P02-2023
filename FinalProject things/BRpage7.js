ac196418c580985185ce29ed6dcdd300abd12dcc
Vinny1027 <gm22rl@brocku.ca>
2023-12-17 21:29:32 -0500

    deleted note

--- FinalProject things/BRpage7.js
+++ FinalProject things/BRpage7.js
@@ -9,12 +9,3 @@ function checkPassword() {
    }
 
 //https://getcodingkids.com/code-skill/create-a-password/
-// function checkPassword() {
-//   var password = document.getElementById("passwordBox");
-//   var passwordText = password.value;
-//   if(passwordText == "3890R6478") {
-//    return true;
-//   }
-//   alert("Password Denied.");
-//   return false;
-//   }
